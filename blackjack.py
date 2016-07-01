1aa3528d1e3aaed0533e40319701266b979cf7cb
Dave Gaulton <dbg2pu@virginia.edu>
2016-07-01 19:19:19 -0400

    Added play again loop

--- blackjack.py
+++ blackjack.py
@@ -2,59 +2,75 @@
 import sys
 from cards import *
 
+
+def play_game():
+	pass
+
+
 def main():
 
-	deck = Deck()
+	play = True
+	
+	while play:
 
-	deck.shuffle()
+		deck = Deck()
 
-	player_hand = Hand()
+		deck.shuffle()
 
-	dealer_hand = Hand()
+		player_hand = Hand()
 
-	player_hand.add(deck.deal())
-	player_hand.add(deck.deal())
+		dealer_hand = Hand()
 
-	dealer_hand.add(deck.deal(), True)
-	dealer_hand.add(deck.deal())
+		player_hand.add(deck.deal())
+		player_hand.add(deck.deal())
 
-	print("Player: " + str(player_hand))
-	print("Dealer: " + str(dealer_hand))
-
-
-	while player_hand.is_under():
-		response = input("Hit or Stand? ").lower()
-
-		if response == "hit":
-			player_hand.add(deck.deal())
-		elif response == "stand":
-			break
-		else:
-			print("Enter \"hit\" or \"stand\"")
-			continue
+		dealer_hand.add(deck.deal(), True)
+		dealer_hand.add(deck.deal())
 
 		print("Player: " + str(player_hand))
-
-	if not player_hand.is_under():
-		print("Bust!")
-		sys.exit()
-
-	else:
-		dealer_hand.reveal_hole_cards()
 		print("Dealer: " + str(dealer_hand))
 
-		while dealer_hand.get_blackjack_val() < 17:
-			dealer_hand.add(deck.deal())
-			time.sleep(1)
+
+		while player_hand.is_under():
+			response = input("Hit or Stand? ").lower()
+
+			if response == "hit" or response == "h":
+				player_hand.add(deck.deal())
+			elif response == "stand" or response == "s":
+				break
+			else:
+				print("Enter \"hit\" or \"stand\"")
+				continue
+
+			print("Player: " + str(player_hand))
+
+		if not player_hand.is_under():
+			print("Bust!")
+			continue
+
+		else:
+			dealer_hand.reveal_hole_cards()
 			print("Dealer: " + str(dealer_hand))
 
-	if not dealer_hand.is_under():
-		print("Dealer busts. You Win!")
-	else:
-		if player_hand.get_blackjack_val() > dealer_hand.get_blackjack_val():
-			print("You Win!")
+			while dealer_hand.get_blackjack_val() < 17:
+				dealer_hand.add(deck.deal())
+				time.sleep(1)
+				print("Dealer: " + str(dealer_hand))
+
+		if not dealer_hand.is_under():
+			print("Dealer busts. You Win!")
 		else:
-			print("you suck")
+			if player_hand.get_blackjack_val() > dealer_hand.get_blackjack_val():
+				print("You Win!")
+			else:
+				print("you suck")
+
+		play_again = input("Play again? ").lower()
+		confirm = ["yes", "y"]
+		if play_again in confirm:
+			play = True
+		else:
+			play = False
 
 
 
