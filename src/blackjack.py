82a3b3ca4d434b57c835e0f2d3beb56c9388858d
Dave Gaulton <dbg2pu@virginia.edu>
2016-07-09 14:50:59 -0400

    Added betting

--- src/blackjack.py
+++ src/blackjack.py
@@ -4,74 +4,102 @@ from cards import *
 
 
 def main():
-	
-	while 1:
-		deck = Deck()
 
-		deck.shuffle()
+    chips = 500
+    print("$" + str(chips) + " buy-in")
 
-		player_hand = Hand()
+    while 1:
 
-		dealer_hand = Hand()
+        if chips == 0:
+            print("You have mortgaged you're house and now have $500")
+            chips = 500
 
-		player_hand.add(deck.deal())
-		player_hand.add(deck.deal())
+        deck = Deck()
 
-		dealer_hand.add(deck.deal(), True)
-		dealer_hand.add(deck.deal())
+        deck.shuffle()
 
-		print("Player: " + str(player_hand))
-		print("Dealer: " + str(dealer_hand))
+        while 1:
+            bet = input("Place your bet: ")
+            if bet.isdigit():
+                bet = int(bet)
+                if bet > chips:
+                    print("You can't bet that much")
+                else:    
+                    break
+            else:
+                print("Enter a number") 
 
-		if (player_hand.get_blackjack_val() == 21):
-			print("You got a blackjack!")
+        player_hand = Hand()
 
-		while player_hand.is_under():
-			response = input("Hit or Stand? ").lower()
+        dealer_hand = Hand()
 
-			if response == "hit" or response == "h":
-				player_hand.add(deck.deal())
-			elif response == "stand" or response == "s":
-				break
-			else:
-				print("Enter \"hit\" or \"stand\"")
-				continue
+        player_hand.add(deck.deal())
+        player_hand.add(deck.deal())
 
-			print("Player: " + str(player_hand))
+        dealer_hand.add(deck.deal(), True)
+        dealer_hand.add(deck.deal())
 
-		if not player_hand.is_under():
-			print("Bust!")
-		else:
-			dealer_hand.reveal_hole_cards()
-			print("Dealer: " + str(dealer_hand))
+        print("Player: " + str(player_hand))
+        print("Dealer: " + str(dealer_hand))
 
-			while dealer_hand.get_blackjack_val() < 17:
-				dealer_hand.add(deck.deal())
-				time.sleep(1)
-				print("Dealer: " + str(dealer_hand))
+        if (player_hand.get_blackjack_val() == 21):
+            print("You got a blackjack!")
 
-			if not dealer_hand.is_under():
-				print("Dealer busts. You win!")
-			else:
-				player_score = player_hand.get_blackjack_val()
-				dealer_score = dealer_hand.get_blackjack_val()
-				print("Player: " + str(player_score) + " Dealer: " + str(dealer_score))
-				if player_score > dealer_score:
-					print("You Win!")
-				elif player_score == dealer_score:
-					print("Push")
-				else:
-					print("you suck")
+        while player_hand.is_under():
+            response = input("Hit or Stand? ").lower()
 
-		while 1:
-			play_again = input("Play again? ").lower()
-			if play_again == "yes" or play_again == "y":
-				break
-			elif play_again == "no" or play_again == "n":
-				sys.exit()
-			else:
-				print("Enter \"yes\" or \"no\"")
+            if response == "hit" or response == "h":
+                player_hand.add(deck.deal())
+            elif response == "stand" or response == "s":
+                break
+            else:
+                print("Enter 'h' or 's'")
+                continue
+
+            print("Player: " + str(player_hand))
+
+        if not player_hand.is_under():
+            print("Bust!")
+            chips -= bet
+        else:
+            dealer_hand.reveal_hole_cards()
+            print("Dealer: " + str(dealer_hand))
+            time.sleep(1)
+
+            while dealer_hand.get_blackjack_val() < 17:
+                dealer_hand.add(deck.deal())
+                print("Dealer: " + str(dealer_hand))
+                time.sleep(1)
+
+            if not dealer_hand.is_under():
+                print("Dealer busts. You win!")
+                chips += bet
+            else:
+                player_score = player_hand.get_blackjack_val()
+                dealer_score = dealer_hand.get_blackjack_val()
+                print("Player: " + str(player_score) + " Dealer: " + str(dealer_score))
+                time.sleep(1)
+                if player_score > dealer_score:
+                    print("You Win!")
+                    chips += bet
+                elif player_score == dealer_score:
+                    print("Push")
+                else:
+                    print("you suck")
+                    chips -= bet
+
+        time.sleep(1)
+
+        while 1:
+            print("You have $" + str(chips))
+            play_again = input("Play again? ").lower()
+            if play_again == "yes" or play_again == "y":
+                break
+            elif play_again == "no" or play_again == "n":
+                sys.exit()
+            else:
+                print("Enter 'y' or 'n'")
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- src/cards.py
+++ src/cards.py
@@ -3,83 +3,83 @@ import random
 import time
 
 SUIT_UNICODE = {
-	"spades": chr(0x2664),
-	"hearts": chr(0x2665),
-	"diamonds": chr(0x2666),
-	"clubs": chr(0x2667)
+    "spades": chr(0x2664),
+    "hearts": chr(0x2665),
+    "diamonds": chr(0x2666),
+    "clubs": chr(0x2667)
 }
 
 CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 
 class Card:
-	def __init__(self, value, suit):
-		self._suit = SUIT_UNICODE[suit]
-		self._value = value
-		self.is_hole = False;
+    def __init__(self, value, suit):
+        self._suit = SUIT_UNICODE[suit]
+        self._value = value
+        self.is_hole = False;
 
-	def __repr__(self):
-		if self.is_hole:
-			return "** "
-		#elif self._value == "10"
-		#	return self._value + self._suit
-		else:
-			return self._value + self._suit + " "
+    def __repr__(self):
+        if self.is_hole:
+            return "** "
+        #elif self._value == "10"
+        #   return self._value + self._suit
+        else:
+            return self._value + self._suit + " "
 
 
 class Deck():
-	def __init__(self):
-		self._cards = []
-		for suit in SUIT_UNICODE:
-			for value in CARD_VALUES:
-				self._cards.append(Card(value, suit))
-		self.discarded_cards = []
+    def __init__(self):
+        self._cards = []
+        for suit in SUIT_UNICODE:
+            for value in CARD_VALUES:
+                self._cards.append(Card(value, suit))
+        self.discarded_cards = []
 
-	def shuffle(self):
-		random.shuffle(self._cards)
+    def shuffle(self):
+        random.shuffle(self._cards)
 
-	def deal(self):
-		return self._cards.pop()
+    def deal(self):
+        return self._cards.pop()
 
-	def __repr__(self):
-		return str(self._cards)
+    def __repr__(self):
+        return str(self._cards)
 
 
 class Hand():
-	def __init__(self):
-		self._cards = []
-		self.hole_cards = []
+    def __init__(self):
+        self._cards = []
+        self.hole_cards = []
 
-	def add(self, card, is_hole=False):
-		card.is_hole = is_hole
-		self._cards.append(card)
-		if is_hole:
-			self.hole_cards.append(card)
+    def add(self, card, is_hole=False):
+        card.is_hole = is_hole
+        self._cards.append(card)
+        if is_hole:
+            self.hole_cards.append(card)
 
-	def __repr__(self):
-		output = ""
-		for card in self._cards:
-			output += str(card) + " "
-		return output
+    def __repr__(self):
+        output = ""
+        for card in self._cards:
+            output += str(card) + " "
+        return output
 
-	def get_blackjack_val(self):
-		tot = 0;
-		for card in self._cards:
-			royals = ['J', 'Q', 'K']
-			if card._value in royals:
-				tot += 10
-			elif card._value != 'A':
-				tot += int(card._value)
-			else:
-				if tot + 11 > 21:
-					tot += 1
-				else:
-					tot += 11
-		return tot
+    def get_blackjack_val(self):
+        tot = 0;
+        for card in self._cards:
+            royals = ['J', 'Q', 'K']
+            if card._value in royals:
+                tot += 10
+            elif card._value != 'A':
+                tot += int(card._value)
+            else:
+                if tot + 11 > 21:
+                    tot += 1
+                else:
+                    tot += 11
+        return tot
 
-	def is_under(self):
-		return self.get_blackjack_val() <= 21
+    def is_under(self):
+        return self.get_blackjack_val() <= 21
 
-	def reveal_hole_cards(self):
-		for card in self.hole_cards:
-			card.is_hole = False
+    def reveal_hole_cards(self):
+        for card in self.hole_cards:
+            card.is_hole = False
