ce79544fd50cf1f82676e6acaa14a6bfa7d22413
Dave Gaulton <dbg2pu@virginia.edu>
2016-10-04 22:51:06 -0400

    Moved much of the driver to its own class and added reusable deck feature

--- src/blackjack.py
+++ src/blackjack.py
@@ -1,107 +1,12 @@
 #!/usr/bin/env python3
 
-import sys
-from cards import *
-
-
-def start_game():
-    chips = 500
-    print("$" + str(chips) + " buy-in")
-
-    while 1:
-        if chips == 0:
-            print("You have mortgaged you're house and now have $500")
-            chips = 500
-
-        deck = Deck()
-
-        deck.shuffle()
-
-        while 1:
-            bet = input("Place your bet: ")
-            if bet.isdigit():
-                bet = int(bet)
-                if bet > chips:
-                    print("You can't bet that much")
-                else:    
-                    break
-            else:
-                print("Enter a number") 
-
-        player_hand = Hand()
-
-        dealer_hand = Hand()
-
-        player_hand.add(deck.deal())
-        player_hand.add(deck.deal())
-
-        dealer_hand.add(deck.deal(), True)
-        dealer_hand.add(deck.deal())
-
-        print("Player: " + str(player_hand))
-        print("Dealer: " + str(dealer_hand))
-
-        while player_hand.is_under():
-            response = input("Hit or Stand? ").lower()
-
-            if response == "hit" or response == "h":
-                player_hand.add(deck.deal())
-            elif response == "stand" or response == "s":
-                break
-            else:
-                print("Enter 'h' or 's'")
-                continue
-
-            print("Player: " + str(player_hand))
-            time.sleep(1)
-
-        if not player_hand.is_under():
-            print("Bust!")
-            chips -= bet
-        else:
-            dealer_hand.reveal_hole_cards()
-            print("Dealer: " + str(dealer_hand))
-            time.sleep(1)
-
-            while dealer_hand.get_blackjack_val() < 17:
-                dealer_hand.add(deck.deal())
-                print("Dealer: " + str(dealer_hand))
-                time.sleep(1)
-
-            if not dealer_hand.is_under():
-                print("Dealer busts. You win!")
-                chips += bet
-            else:
-                player_score = player_hand.get_blackjack_val()
-                dealer_score = dealer_hand.get_blackjack_val()
-                print("Player: " + str(player_score) + " Dealer: " + str(dealer_score))
-                time.sleep(1)
-                if player_score > dealer_score:
-                    print("You Win!")
-                    chips += bet
-                elif player_score == dealer_score:
-                    print("Push")
-                else:
-                    print("you lose")
-                    chips -= bet
-
-        time.sleep(1)
-
-        print("You have $" + str(chips))
-        while 1:
-            play_again = input("Play again? ").lower()
-            if play_again == "yes" or play_again == "y":
-                break
-            elif play_again == "no" or play_again == "n":
-                sys.exit()
-            else:
-                print("Enter 'y' or 'n'")
-
-
+import cards
 
 def main():
-    start_game()
+    game = cards.BlackjackGame(500)
 
+    while game.start_round():
+        pass
 
 if __name__ == "__main__":
     main()
--- src/cards.py
+++ src/cards.py
@@ -11,14 +11,6 @@ SUIT_UNICODE = {
 
 CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-ALL_HANDS = []
-
-
-def fold_all(deck):
-    for hand in ALL_HANDS:
-        hand.fold(deck)
-
-
 class Card:
     def __init__(self, value, suit):
         self._suit = SUIT_UNICODE[suit]
@@ -28,11 +20,8 @@ class Card:
     def __repr__(self):
         if self.is_hole:
             return "** "
-        #elif self._value == "10"
-        #   return self._value + self._suit
         else:
             return self._value + self._suit + " "
-
 
 class Deck():
     def __init__(self):
@@ -40,17 +29,22 @@ class Deck():
         for suit in SUIT_UNICODE:
             for value in CARD_VALUES:
                 self._cards.append(Card(value, suit))
+        self.shuffle()
+
         self.discarded_cards = []
 
     def shuffle(self):
         random.shuffle(self._cards)
 
-    def deal(self):
+    def deal_card(self):
+        if not self._cards:
+            self._cards = self.discarded_cards.copy()
+            self.discarded_cards.clear()
+            self.shuffle()
         return self._cards.pop()
 
     def __repr__(self):
         return str(self._cards)
-
 
 class Hand():
     def __init__(self):
@@ -92,10 +86,135 @@ class Hand():
             card.is_hole = False
 
     def discard(self, card, deck):
+        card.is_hole = False
         self._cards.remove(card)
         deck.discarded_cards.append(card)
 
     def fold(self, deck):
-        for card in self._cards:
-            discard(card, deck)
+        self.reveal_hole_cards()
+        deck.discarded_cards.extend(self._cards.copy())
+        self._cards.clear()
 
+class Player:
+    def __init__(self, buy_in):
+        self.chips = buy_in
+        self.bet = 0
+        self.hand = Hand()
+
+    def place_bet(self):
+        while True:    
+            response = input("Place your bet: ")
+            if response.isdigit():
+                response = int(response)
+                if response > self.chips:
+                    print("You can't bet that much")
+                else:    
+                    self.bet = response
+                    return self.bet
+            else:
+                print("Enter a number") 
+
+class BlackjackGame:
+    def __init__(self, buy_in):
+        print("$" + str(buy_in) + " buy-in")
+        self.dealer_hand = Hand()
+        self.deck = Deck()
+        self.buy_in = buy_in
+        self.player = Player(self.buy_in)
+
+    def start_round(self):
+        if not self.player.chips:
+            print("You have mortgaged you're house and now have $500")
+            self.player.chips = 500
+
+        self.player.place_bet()
+        
+        self._deal_cards()
+
+        player_bust = self._prompt_player()
+
+        if player_bust:
+            print("Bust!")
+            self.player.chips -= self.player.bet
+            return self.end_game()
+
+        dealer_bust = self._deal_with_dealer()
+
+        if dealer_bust:
+            print("Dealer busts. You win!")
+            self.player.chips += self.player.bet
+            return self.end_game()
+
+        player_score = self.player.hand.get_blackjack_val()
+        dealer_score = self.dealer_hand.get_blackjack_val()
+        print("Player: " + str(player_score) + " Dealer: " + str(dealer_score))
+        time.sleep(1)
+        if player_score > dealer_score:
+            print("You Win!")
+            self.player.chips += self.player.bet
+        elif player_score == dealer_score:
+            print("Push")
+        else:
+            print("you lose")
+            self.player.chips += self.player.bet
+
+        return self.end_game()
+
+    def _deal_cards(self):
+        self.player.hand.add(self.deck.deal_card())
+        self.player.hand.add(self.deck.deal_card())
+
+        self.dealer_hand.add(self.deck.deal_card(), True)
+        self.dealer_hand.add(self.deck.deal_card())
+
+        print("Player: " + str(self.player.hand))
+        print("Dealer: " + str(self.dealer_hand))
+
+
+    def _prompt_player(self):
+        while self.player.hand.is_under():
+            response = input("Hit or Stand? ").lower()
+
+            if response == "hit" or response == "h":
+                self.player.hand.add(self.deck.deal_card())
+            elif response == "stand" or response == "s":
+                return False
+            else:
+                print("Enter 'h' or 's'")
+                continue
+
+            print("Player: " + str(self.player.hand))
+            time.sleep(1)
+
+        return True
+
+    def _deal_with_dealer(self):
+        self.dealer_hand.reveal_hole_cards()
+        print("Dealer: " + str(self.dealer_hand))
+        time.sleep(1)
+
+        while self.dealer_hand.get_blackjack_val() < 17:
+            self.dealer_hand.add(self.deck.deal_card())
+            print("Dealer: " + str(self.dealer_hand))
+            time.sleep(1)
+
+        return not self.dealer_hand.is_under()
+
+    def end_game(self):
+        self._turn_in_cards()
+        return self._ask_play_again()
+
+    def _turn_in_cards(self):
+        self.player.hand.fold(self.deck)
+        self.dealer_hand.fold(self.deck)
+
+    def _ask_play_again(self):
+        print("You have $" + str(self.player.chips))
+        while True:
+            play_again = input("Play again? ").lower()
+            if play_again == "yes" or play_again == "y":
+                return True
+            elif play_again == "no" or play_again == "n":
+                return False
+            else:
+                print("Enter 'y' or 'n'")
